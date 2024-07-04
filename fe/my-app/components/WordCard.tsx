4a1414716b9d309dc3e6e8bb4bd867a4ecf8cc41
JaeHyeongPark <kidsjohn960526@gmail.com>
2024-07-05 00:25:36 +0900

    chore: change text color into black

--- fe/my-app/components/WordCard.tsx
+++ fe/my-app/components/WordCard.tsx
@@ -19,17 +19,10 @@ const WordCard: React.FC<WordCardProps> = ({
   examples,
 }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-2xl mx-auto my-6">
+    <div className="bg-white shadow-md rounded-lg text-black p-6 w-full max-w-2xl mx-auto my-6">
       <div className="flex justify-between items-center">
         <h2 className="text-3xl font-bold text-blue-700">{word}</h2>
-        <div className="flex space-x-2">
-          <button className="text-gray-500 hover:text-gray-700">
-            <i className="fab fa-twitter"></i>
-          </button>
-          <button className="text-gray-500 hover:text-gray-700">
-            <i className="fab fa-facebook"></i>
-          </button>
-        </div>
+        <div className="flex space-x-2"></div>
       </div>
       <div className="mt-2 flex items-center text-xl text-gray-700">
         <SpeakerIcon />
