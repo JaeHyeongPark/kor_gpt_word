55d39d26560b531407e3720e836717920c93935f
JaeHyeongPark <kidsjohn960526@gmail.com>
2024-07-09 14:24:58 +0900

    chore: middleware

--- fe/my-app/middleware.ts
+++ fe/my-app/middleware.ts
@@ -34,24 +34,7 @@ export async function middleware(req: NextRequest) {
       }
 
       if (userAnswers?.answers_completed) {
-        const { data: userWords, error: userWordsError } = await supabase
-          .from("user_words")
-          .select("id")
-          .eq("user_id", session.user.id)
-          .limit(1);
-
-        if (userWordsError) {
-          console.error("Error fetching user words:", userWordsError.message);
-          return res;
-        }
-
-        if (userWords.length > 0) {
-          console.log("user has history");
-          return NextResponse.redirect(new URL("/practice", req.url));
-        } else {
-          console.log("user has completed answers but no history");
-          return NextResponse.redirect(new URL("/countdown", req.url));
-        }
+        return NextResponse.redirect(new URL("/practice", req.url));
       } else {
         console.log("user has not completed answers");
         return NextResponse.redirect(new URL("/question/1", req.url));
@@ -72,24 +55,7 @@ export async function middleware(req: NextRequest) {
       }
 
       if (userAnswers?.answers_completed) {
-        const { data: userWords, error: userWordsError } = await supabase
-          .from("user_words")
-          .select("id")
-          .eq("user_id", session.user.id)
-          .limit(1);
-
-        if (userWordsError) {
-          console.error("Error fetching user words:", userWordsError.message);
-          return res;
-        }
-
-        if (userWords.length > 0) {
-          console.log("user has history");
-          return NextResponse.redirect(new URL("/practice", req.url));
-        } else {
-          console.log("user has completed answers but no history");
-          return NextResponse.redirect(new URL("/countdown", req.url));
-        }
+        return NextResponse.redirect(new URL("/practice", req.url));
       } else {
         console.log("user has not completed answers");
         return NextResponse.redirect(new URL("/question/1", req.url));
